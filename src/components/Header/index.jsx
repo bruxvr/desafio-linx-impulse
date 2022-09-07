72e69bc6450079b9f6be839ddca8738213a510fd
Bruna Xavier <brunaxavier@id.uff.br>
2022-09-07 11:21:00 -0300

    feat: deleção de arquivos desnecessários

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import { Button } from "../Button"
+
+
+export function Header () {
+  return (
+    <header>
+      <h1>Header</h1>
+      <Button>qualquer coisa</Button>
+      <Button>hello gabriel</Button>
+
+    </header>
+  )
+}
